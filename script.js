c568ca419cd66d5c6d38f0a209177318bcc8f5e0
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-03-03 23:32:57 +0000

    Refactor function and add car object

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// Run: tsc script.ts
-console.log('Hello TypeScript!');
-var hello = function (name, country) {
-    console.log("Hello ".concat(name, " from ").concat(country));
-};
-hello('John', 'USA');
-console.log('ts installed!');
+
--- script.ts
+++ script.ts
@@ -1,11 +1,18 @@
 // Run: tsc script.ts
 console.log('Hello TypeScript!');
 
-const hello = (name, country) => {
+function Hi(name: string, country: string) {
     console.log(`Hello ${name} from ${country}`);
-};
+}
 
-hello('John', 'USA');
+Hi('John', 'USA');
 
 console.log('ts installed!');
+
+const car = {
+    brand: 'BMW',
+    engine: 3.0,
+};
+
+console.log(car.engine);
 
