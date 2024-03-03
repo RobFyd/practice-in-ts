59fe0426cc8e7267342149a4cde61bb7ce3a926b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-03-03 23:54:06 +0000

    Refactor script.js and script.ts es2015

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 // Run: tsc script.ts
 console.log('Hello TypeScript!');
-function Hi(name, country) {
-    console.log("Hello ".concat(name, " from ").concat(country));
-}
-Hi('John', 'USA');
-console.log('ts installed!');
-var car = {
-    brand: 'BMW',
-    engine: 3.0,
+const hello = (name, country) => {
+    console.log(`Hello ${name} from ${country}`);
 };
-console.log(car.engine);
-var adultness = age >= 18 ? 'adult' : 'minor';
-switch (adultness) {
-    case "adult":
-}
+hello('John', 'USA');
--- script.ts
+++ script.ts
@@ -1,24 +1,9 @@
 // Run: tsc script.ts
 console.log('Hello TypeScript!');
 
-function Hi(name: string, country: string) {
+const hello = (name: string, country: string) => {
     console.log(`Hello ${name} from ${country}`);
-}
-
-Hi('John', 'USA');
-
-console.log('ts installed!');
-
-const car = {
-    brand: 'BMW',
-    engine: 3.0,
 };
 
-console.log(car.engine);
-
-const adultness = age >= 18 ? 'adult' : 'minor';
-
-switch (adultness) {
-    case "adult":
-}
+hello('John', 'USA');
 
