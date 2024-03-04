35c2f8d8c3b812c1e17f0fd787bab7fe05486372
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-03-04 00:01:55 +0000

    Add strict mode and update TypeScript target

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+"use strict";
 // Run: tsc script.ts
 console.log('Hello TypeScript!');
 const hello = (name, country) => {
     console.log(`Hello ${name} from ${country}`);
 };
 hello('John', 'USA');
+
+// tsc script.ts --target es2015 --strict
