cb9151e8f47e623d0875944d536422ee55bed842
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-03-05 00:17:41 +0000

    Refactor script.js and script.ts to include type annotations and add new functions

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-"use strict";
 // Run: tsc script.ts
 console.log('Hello TypeScript!');
-const hello = (name, country) => {
-    console.log(`Hello ${name} from ${country}`);
+var hello = function (name, country) {
+    console.log("Hello ".concat(name, " from ").concat(country));
 };
 hello('John', 'USA');
-
-// tsc script.ts --target es2015 --strict
+// tsc script.ts --target es2015 --strict - to compile with es2015 and strict mode
+// types
+// type primitive (number, string, boolean, null, undefined, symbol, void):
+var getArea = function (width, height) { return width * height; };
+console.log(getArea(5, 10));
+// type array:
+var numbers = [1, 2, 3, 4, 5]; // number[]
+// type any:
+var car = {
+    brand: 'BMW'
+};
+car = 'Mercedes';
+car.year = 2020; // no error, try avoid using any
+// type function:
+var welcome = function (name, age) {
+    console.log("Hello ".concat(name, ", you are ").concat(age, " years old"));
+};
+welcome('John', 25);
--- script.ts
+++ script.ts
@@ -11,8 +11,33 @@ hello('John', 'USA');
 
 // types
 
-// type primitive:
+
+// type primitive (number, string, boolean, null, undefined, symbol, void):
 
 const getArea = (width: number, height: number) => width * height;
 
 console.log(getArea(5, 10));
+
+
+// type array:
+
+const numbers = [1, 2, 3, 4, 5]; // number[]
+
+
+// type any:
+
+let car: any = {
+    brand: 'BMW'
+};
+
+car = 'Mercedes';
+car.year = 2020;     // no error, try avoid using any
+
+
+// type function:
+
+const welcome = (name: string, age: number) => {
+    console.log(`Hello ${name}, you are ${age} years old`);
+}
+
+welcome('John', 25);
