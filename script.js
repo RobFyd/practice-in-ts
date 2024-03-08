2b6f8ec9cbedbdfbf2993311086b32bda6a1fdce
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-03-08 00:13:54 +0000

    code refactored

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 // Run: tsc script.ts
+// Output: script.js
 console.log("Hello TypeScript!");
 var hello = function (name, country) {
     console.log("Hello ".concat(name, " from ").concat(country));
@@ -8,87 +9,111 @@ hello("John", "USA");
 // tsc script.ts --target es2015 --strict - to compile with es2015 and strict mode
 // types
 // type primitive (number, string, boolean, null, undefined, symbol, void):
-var getArea = function (width, height) { return width * height; };
-console.log(getArea(5, 10));
+{
+    var getArea = function (width, height) { return width * height; };
+    console.log(getArea(5, 10));
+}
 // type array:
-var numbers = [1, 2, 3, 4, 5]; // number[]
+{
+    var numbers = [1, 2, 3, 4, 5]; // number[]
+}
 // type any:
-var car = {
-    brand: "BMW",
-};
-// car = "Mercedes";
-// car.year = 2020; // no error, try avoid using any
+{
+    var car = {
+        brand: "BMW",
+    };
+    // car = "Mercedes";
+    // car.year = 2020; // no error, try avoid using any
+}
 // type function:
-var welcome = function (name, age) {
-    console.log("Hello ".concat(name, ", you are ").concat(age, " years old"));
-};
-welcome("John", 25);
-var getEUR = function (plnAmount, rate) { return plnAmount / rate; };
-console.log(getEUR(100, 4.71));
-var bye = function () {
-    console.log("Goodbye!");
-};
-var newNumber = [12321];
-newNumber.forEach(function (number) {
-    console.log(number);
-});
+{
+    var welcome = function (name, age) {
+        console.log("Hello ".concat(name, ", you are ").concat(age, " years old"));
+    };
+    welcome("John", 25);
+    var getEUR = function (plnAmount, rate) { return plnAmount / rate; };
+    console.log(getEUR(100, 4.71));
+    var bye = function () {
+        console.log("Goodbye!");
+    };
+    var newNumber = [12321];
+    newNumber.forEach(function (number) {
+        console.log(number);
+    });
+}
 // type object:
-var newHello = function (person) {
-    console.log("Hello ".concat(person.name).concat(person.age ? ", you are ".concat(person.age, " years old") : "", "!"));
-};
-newHello({ name: "Bob", age: 45 }); // age? - optional parameter
+{
+    var newHello = function (person) {
+        console.log("Hello ".concat(person.name).concat(person.age ? ", you are ".concat(person.age, " years old") : "", "!"));
+    };
+    newHello({ name: "Bob", age: 45 }); // age? - optional parameter
+}
 // when the value has two different types:
-var writeToConsole = function (value) {
-    if (typeof value === "string") {
-        console.log(value.toLocaleUpperCase());
-        return;
-    }
-    console.log(value.toFixed(2));
-};
-writeToConsole("Hello");
-writeToConsole(123);
-// all types have a method which is needed
-var getLenght = function (value) { return value.length; };
-console.log(getLenght([1, 2, 3, 4, 5]));
-console.log(getLenght("Hello"));
-// interface ICar {            // the same as type often with "I" prefix
-//     brand: string;
-//     year: number;
-// }
-var car2 = {
-    brand: "BMW",
-    year: 2020,
-};
-console.log(car2.brand, car2.year);
-var showCar = function (car2) {
-    console.log("Brand: ".concat(car2.brand, ", year: ").concat(car.year));
-};
-showCar(car2);
-var dog = {
-    bark: function () {
-        console.log("Woof!");
-    },
-    name: "Burek",
-};
-console.log(dog.name);
-console.log(dog.bark());
-// type assertion:
-var nameInputElement = document.querySelector(".js-name");
-var motorbike = {
-    brand: "Yamaha",
-    // color: "white", // error
-    color: "red",
-    city: "Warsaw",
-    new: true,
-}; // typescript knows that the object is constant
-console.log(motorbike.brand, motorbike.color, motorbike.city, motorbike.new);
-var buyMotorbike = function (motorbike) { };
-buyMotorbike(motorbike);
+{
+    var writeToConsole = function (value) {
+        if (typeof value === "string") {
+            console.log(value.toLocaleUpperCase());
+            return;
+        }
+        console.log(value.toFixed(2));
+    };
+    writeToConsole("Hello");
+    writeToConsole(123);
+    // all types have a method which is needed
+    var getLenght = function (value) { return value.length; };
+    console.log(getLenght([1, 2, 3, 4, 5]));
+    console.log(getLenght("Hello"));
+}
+// alias type:
+{
+    // interface ICar {            // the same as type often with "I" prefix
+    //     brand: string;
+    //     year: number;
+    // }
+    var car2 = {
+        brand: "BMW",
+        year: 2020,
+    };
+    console.log(car2.brand, car2.year);
+    var showCar = function (car2) {
+        console.log("Brand: ".concat(car2.brand, ", year: ").concat(car2.year));
+    };
+    showCar(car2);
+}
+// alias type interface:
+{
+    var dog = {
+        bark: function () {
+            console.log("Woof!");
+        },
+        name: "Burek",
+    };
+    console.log(dog.name);
+    console.log(dog.bark());
+    // type assertion:
+    var nameInputElement = document.querySelector(".js-name");
+    // it helps typescript to understand that it is an input element
+}
+// literal types:
+{
+    var motorbike = {
+        brand: "Yamaha",
+        // color: "white", // error
+        color: "red",
+        city: "Warsaw",
+        new: true,
+    }; // typescript knows that the object is constant
+    console.log(motorbike.brand, motorbike.color, motorbike.city, motorbike.new);
+    var buyMotorbike = function (motorbike) { };
+    buyMotorbike(motorbike);
+}
 // null and undefined:
-var hideElement = function (element) {
-    element.classList.add("hidden");
-};
-var element = document.querySelector(".js-element");
-if (element !== null) {
-    hideElement(element);
+{
+    var hideElement = function (element) {
+        element.classList.add("hidden");
+    };
+    var element = document.querySelector(".js-element");
+    if (element !== null) {
+        hideElement(element);
+    }
 }
--- script.ts
+++ script.ts
@@ -1,4 +1,6 @@
 // Run: tsc script.ts
+// Output: script.js
+
 console.log("Hello TypeScript!");
 
 const hello = (name: string, country: string) => {
@@ -11,172 +13,183 @@ hello("John", "USA");
 
 // types
 
-
 // type primitive (number, string, boolean, null, undefined, symbol, void):
+{
+  const getArea = (width: number, height: number) => width * height;
 
-const getArea = (width: number, height: number) => width * height;
-
-console.log(getArea(5, 10));
+  console.log(getArea(5, 10));
+}
 
 
 // type array:
-
-const numbers = [1, 2, 3, 4, 5]; // number[]
+{
+  const numbers = [1, 2, 3, 4, 5]; // number[]
+}
 
 
 // type any:
+{
+  let car: any = {
+    brand: "BMW",
+  };
 
-let car: any = {
-  brand: "BMW",
-};
-
-// car = "Mercedes";
-// car.year = 2020; // no error, try avoid using any
+  // car = "Mercedes";
+  // car.year = 2020; // no error, try avoid using any
+}
 
 
 // type function:
+{
+  const welcome = (name: string, age: number) => {
+    console.log(`Hello ${name}, you are ${age} years old`);
+  };
 
-const welcome = (name: string, age: number) => {
-  console.log(`Hello ${name}, you are ${age} years old`);
-};
+  welcome("John", 25);
 
-welcome("John", 25);
+  const getEUR = (plnAmount: number, rate: number): number => plnAmount / rate;
 
-const getEUR = (plnAmount: number, rate: number): number => plnAmount / rate;
+  console.log(getEUR(100, 4.71));
 
-console.log(getEUR(100, 4.71));
+  const bye = (): void => {
+    console.log("Goodbye!");
+  };
 
-const bye = (): void => {
-  console.log("Goodbye!");
-};
+  const newNumber = [12321];
 
-const newNumber = [12321];
-
-newNumber.forEach((number) => {
-  console.log(number);
-});
+  newNumber.forEach((number) => {
+    console.log(number);
+  });
+}
 
 
 // type object:
+{
+  const newHello = (person: { name: string; age?: number }) => {
+    console.log(
+      `Hello ${person.name}${
+        person.age ? `, you are ${person.age} years old` : ""
+      }!`
+    );
+  };
 
-const newHello = (person: { name: string; age?: number }) => {
-  console.log(
-    `Hello ${person.name}${
-      person.age ? `, you are ${person.age} years old` : ""
-    }!`
-  );
-};
-
-newHello({ name: "Bob", age: 45 }); // age? - optional parameter
+  newHello({ name: "Bob", age: 45 }); // age? - optional parameter
+}
 
 
 // when the value has two different types:
-
-const writeToConsole = (value: string | number) => {
-  if (typeof value === "string") {
-    console.log(value.toLocaleUpperCase());
-    return;
-  }
+{
+  const writeToConsole = (value: string | number) => {
+    if (typeof value === "string") {
+      console.log(value.toLocaleUpperCase());
+      return;
+    }
 
     console.log(value.toFixed(2));
-};
+  };
 
-writeToConsole("Hello");
-writeToConsole(123);
+  writeToConsole("Hello");
+  writeToConsole(123);
 
+  // all types have a method which is needed
 
-// all types have a method which is needed
+  const getLenght = (value: number[] | string) => value.length;
 
-const getLenght = (value: number[] | string) => value.length;
-
-console.log(getLenght([1, 2, 3, 4, 5]));
-console.log(getLenght("Hello"));
+  console.log(getLenght([1, 2, 3, 4, 5]));
+  console.log(getLenght("Hello"));
+}
 
 
 // alias type:
-
-type Car = {
+{
+  type Car = {
     brand: string;
     year: number;
-};
+  };
 
-// interface ICar {            // the same as type often with "I" prefix
-//     brand: string;
-//     year: number;
-// }
+  // interface ICar {            // the same as type often with "I" prefix
+  //     brand: string;
+  //     year: number;
+  // }
 
-const car2: Car = {
+  const car2: Car = {
     brand: "BMW",
     year: 2020,
-};
+  };
 
-console.log(car2.brand, car2.year);
+  console.log(car2.brand, car2.year);
 
-const showCar = (car2: Car) => {
-    console.log(`Brand: ${car2.brand}, year: ${car.year}`);
-};
+  const showCar = (car2: Car) => {
+    console.log(`Brand: ${car2.brand}, year: ${car2.year}`);
+  };
 
-showCar(car2);
+  showCar(car2);
+}
+
 
 // alias type interface:
-
-interface Animal {
+{
+  interface Animal {
     name: string;
-}
+  }
 
-interface Dog extends Animal {
+  interface Dog extends Animal {
     bark: () => void;
-}
+  }
 
-const dog: Dog = {
+  const dog: Dog = {
     bark: () => {
-        console.log("Woof!");
+      console.log("Woof!");
     },
     name: "Burek",
+  };
+
+  console.log(dog.name);
+  console.log(dog.bark());
+
+
+  // type assertion:
+
+  const nameInputElement = document.querySelector(
+    ".js-name"
+  ) as HTMLInputElement;
+  // it helps typescript to understand that it is an input element
 }
-
-console.log(dog.name);
-console.log(dog.bark());
-
-
-// type assertion:
-
-const nameInputElement = document.querySelector(".js-name") as HTMLInputElement;
-// it helps typescript to understand that it is an input element
 
 
 // literal types:
-
-interface Motorbike {
+{
+  interface Motorbike {
     brand: string;
     color: "red" | "blue" | "green";
     city: "Warsaw" | "Berlin" | "Paris";
-    new: boolean;  // it same as new: true | false;
-}
+    new: boolean; // it same as new: true | false;
+  }
 
-const motorbike = {
+  const motorbike = {
     brand: "Yamaha",
     // color: "white", // error
     color: "red",
     city: "Warsaw",
     new: true,
-} as const;     // typescript knows that the object is constant
+  } as const; // typescript knows that the object is constant
 
-console.log(motorbike.brand, motorbike.color, motorbike.city, motorbike.new);
+  console.log(motorbike.brand, motorbike.color, motorbike.city, motorbike.new);
 
-const buyMotorbike = (motorbike: Motorbike) => {};
+  const buyMotorbike = (motorbike: Motorbike) => {};
 
-buyMotorbike(motorbike);
+  buyMotorbike(motorbike);
+}
 
 
 // null and undefined:
-
-const hideElement = (element: Element) => {
+{
+  const hideElement = (element: Element) => {
     element.classList.add("hidden");
-};
+  };
 
-const element = document.querySelector(".js-element");
+  const element = document.querySelector(".js-element");
 
-if (element !== null) {
+  if (element !== null) {
     hideElement(element);
+  }
 }
