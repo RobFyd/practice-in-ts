31e41e206a33688e9af0b86ec91653ec492fe0f5
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-03-08 00:21:29 +0000

    demonstrate narrow type assertion in TypeScript

--- scriptNarrow.js
+++ scriptNarrow.js
@@ -1,9 +1,24 @@
 "use strict";
-var writeToConsole2 = function (value) {
-    if (typeof value === "number") {
-        console.log(value.toFixed(2));
-    }
-    else {
-        console.log(value.toLocaleUpperCase());
-    }
-};
+// Purpose: This file contains code that demonstrates how to use the narrow type assertion in TypeScript.
+// typeof - Narrowing by type
+{
+    var writeToConsole = function (value) {
+        if (typeof value === "number") {
+            console.log(value.toFixed(2));
+        }
+        else {
+            console.log(value.toLocaleUpperCase());
+        }
+    };
+    writeToConsole(5.123321);
+    writeToConsole("Hello World");
+}
+// typeof for null gives "object"
+{
+    var getAge = function (person) {
+        if (typeof person === "object") {
+            return person === null || person === void 0 ? void 0 : person.age;
+        }
+    };
+    console.log(getAge({ age: 45 }));
+}
--- scriptNarrow.ts
+++ scriptNarrow.ts
@@ -1,7 +1,26 @@
-const writeToConsole2 = (value: number | string) => {
-  if (typeof value === "number") {
-    console.log(value.toFixed(2));
-  } else {
-    console.log(value.toLocaleUpperCase());
-  }
-};
+// Purpose: This file contains code that demonstrates how to use the narrow type assertion in TypeScript.
+
+// typeof - Narrowing by type
+{
+  const writeToConsole = (value: number | string) => {
+    if (typeof value === "number") {
+      console.log(value.toFixed(2));
+    } else {
+      console.log(value.toLocaleUpperCase());
+    }
+  };
+
+  writeToConsole(5.123321);
+  writeToConsole("Hello World");
+}
+
+// typeof for null gives "object"
+{
+  const getAge = (person: { age: number } | null) => {
+    if (typeof person === "object") {
+      return person?.age;
+    }
+  };
+
+    console.log(getAge({ age: 45 }));
+}
