b228461b5cc08a489e6452aeba416cdc7c022c3f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-03-13 23:55:46 +0000

    Add function typing to scriptFunctions.js

--- scriptFunctions.js
+++ scriptFunctions.js
@@ -1,2 +1,12 @@
 "use strict";
 console.log("in operator");
+// function typing
+{
+    const greeter = (greetFunctions) => {
+        greetFunctions("Hello, World!");
+    };
+    const printToConsole = (text) => {
+        console.log(text);
+    };
+    greeter(printToConsole);
+}
--- scriptFunctions.ts
+++ scriptFunctions.ts
@@ -1,1 +1,14 @@
 console.log("in operator");
+
+// function typing
+{
+  const greeter = (greetFunctions: (text: string) => void) => {
+    greetFunctions("Hello, World!");
+  };
+
+  const printToConsole = (text: string) => {
+    console.log(text);
+  };
+
+  greeter(printToConsole);
+}
